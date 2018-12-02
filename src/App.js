90ea28f90806476e0bf06313858fcfbdff560b95
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2018-12-02 19:19:38 +0900

    Grid size determined by variables

--- src/App.js
+++ src/App.js
@@ -16,13 +16,19 @@ class App extends React.Component {
   }
 
   render() {
+    const nRow = 2;
+    const nColumn = 2;
+
+    const rowStyle = nRow === 1 ? styles.row1 : styles.row2;
+    const colStyle = nColumn === 1 ? styles.col1 : styles.col2;
+
     return (
       <div className={styles.wrapper}>
-        <div className={styles.parent}>
+        <div className={`${styles.parent} ${colStyle} ${rowStyle}`}>
           <DrawToolbarExample ref={this.maps[0]} maps={this.maps} />
-          <DraggableExample ref={this.maps[1]} maps={this.maps} />
-          <ToolbarExample ref={this.maps[2]} maps={this.maps} />
-          <SimpleExample ref={this.maps[3]} maps={this.maps} />
+          {nColumn === 2 && <DraggableExample ref={this.maps[1]} maps={this.maps} />}
+          {nRow === 2 && <ToolbarExample ref={this.maps[2]} maps={this.maps} />}
+          {nRow === 2 && nColumn === 2 && <SimpleExample ref={this.maps[3]} maps={this.maps} />}
         </div>
         <div className={styles.orphan}>
           <Azavea />
